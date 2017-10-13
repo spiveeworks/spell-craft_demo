410fdc2f14911129ea6f87f91d9d33c59d1f0a77
SpiveeWorks <spiveehere@gmail.com>
2017-10-13 20:46:22 +1100

    complicated structs

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,31 @@ extern crate piston_window;
 
 use piston_window::*;
 
+mod game;
+
+struct Player {
+    body: game::Body,
+}
+
+struct Game {
+    time: game::Time,
+    player: Player,
+}
+
+impl Game {
+    fn new(player_loc: game::Position) -> Game {
+        let time = 0;
+        let body = game::Body::new(
+            player_loc,      // initial location
+            game::ZERO_VEC,  // stationary
+            time             // any time works since stationary
+        );
+        let player = Player { body };
+        Game { time, player }
+    }
+}
+
+
 fn settings() -> WindowSettings {
     WindowSettings::new(
         "piston-tutorial",
@@ -10,6 +35,7 @@ fn settings() -> WindowSettings {
 }
 
 fn main() {
+    let game_state = Game::new(game::ZERO_VEC);
     let mut window: PistonWindow =
         settings().exit_on_esc(true)
                   .build()
