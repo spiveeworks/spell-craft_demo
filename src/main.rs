5629184216aed7a618235e77532ff420f78467aa
SpiveeWorks <spiveehere@gmail.com>
2017-10-13 18:09:40 +1100

    window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate piston_window;
+
+use piston_window::*;
+
+fn settings() -> WindowSettings {
+    WindowSettings::new(
+        "piston-tutorial",
+        [600, 600]
+    )
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut window: PistonWindow =
+        settings().exit_on_esc(true)
+                  .build()
+                  .expect("Failed to build window from settings.");
+    while let Some(_e) = window.next() {
+
+    }
 }
